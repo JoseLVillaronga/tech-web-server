0301658937d6366c4cd5123c54a823e44ae2d1f2
José Luis Villaronga <jlvillaronga@teccam.net>
2025-06-20 12:13:46 -0300

    Update

--- public/info.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "<h1>PHP Info - localhost</h1>";
-echo "<p>PHP Version: " . phpversion() . "</p>";
-echo "<p>Server: " . $_SERVER['SERVER_NAME'] . "</p>";
-echo "<p>Document Root: " . $_SERVER['DOCUMENT_ROOT'] . "</p>";
-?>
